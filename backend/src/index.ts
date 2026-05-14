811e29636679d5b05fbc284bfeb7adb4083d0bb8
kingfrog711 <gunata.sakri@gmail.com>
2026-05-14 19:47:38 +0700

    nambah frontendurl

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,6 +26,7 @@ app.use(helmet());
 const allowedOrigins = [
   process.env.FRONTEND_URL || "http://localhost:5173",
   "http://localhost:5173",
+  "http://localhost:5174",
 ];
 app.use(cors({
   origin: (origin, callback) => {
